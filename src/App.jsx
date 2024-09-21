a000c02870a6eb6d8c706fb5ec51ef5fcabe9f55
Jack M <jackmcgrath2k@gmail.com>
2024-09-21 17:39:54 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <p className="text-red-600">hello</p>
+  )
+}
+
+export default App
